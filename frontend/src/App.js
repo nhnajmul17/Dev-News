65fe79bbecd51ee640ab55c426fa3310fe9e54b8
nhnajmul17 <nhnajmul17@gmail.com>
2022-01-02 14:06:15 +0600

    React Route Tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import PythonNews from './Components/AllNews/PythonNews/PythonNews';
 import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <div>
+          <h1>Welcome to News</h1>
+
+          <ul>
+            <Link to='/home'> <li>Home</li> </Link>
+            <Link to='/pythonnews'>  <li>Python News</li> </Link>
+            <Link to='/login'><li>Login</li> </Link>
+          </ul>
+        </div>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='home' element={<Home />} />
+          <Route path='pythonnews' element={<PythonNews />} />
+          <Route path='login' element={<Login />} />
         </Routes>
       </BrowserRouter>
 
---
+++ frontend/src/Components/AllNews/PythonNews/PythonNews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PythonNews = () => {
+    return (
+        <div>
+            <h3>This is Python News</h3>
+        </div>
+    );
+};
+
+export default PythonNews;
