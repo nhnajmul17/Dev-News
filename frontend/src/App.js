c53cf11d389e704833b90e3f07f39c43b5307fc9
naime-ahmed <naime00ahmed@gmail.com>
2022-01-04 11:22:27 +0600

    initial details news page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,20 @@ import About from './Components/About/About';
 import Allnews from './Components/AllNews/Allnews';
 
 import Contact from './Components/Contact/Contact';
+import DetailsNews from './Components/Home/DetailsNews/DetailsNews';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
 import Error from './Components/Shared/Error/Error';
-import Header from './Components/Shared/Header/Header';
 import Signup from './Components/SignUp/Signup';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header></Header>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='home' element={<Home />} />
+          <Route path='home/:newsId' element={<DetailsNews/>}/>
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
           <Route path='allnews' element={<Allnews />} />
--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -5,18 +5,18 @@ import Footer from '../Shared/Footer/Footer';
 import Brands from './Brands/Brands';
 import TopNews from './TopNews/TopNews';
 import EventSlider from './Events/EventSlider/EventSlider';
+import Header from '../Shared/Header/Header';
 
 const Home = () => {
     return (
         <div>
-            
+            <Header></Header>
+            <Banner></Banner>
             <TopNews />
+            <Publishing></Publishing>
             <EventSlider></EventSlider>
             <Brands />
             <Footer/>
-            <EventSlider></EventSlider>
-            <Banner></Banner>
-            <Publishing></Publishing>
         </div>
     );
 };
