9a5d7bddd62dbdbb4ce1557e76c577e6150175e5
rakinul-islam-raju <rakibul.islam7772588@gmail.com>
2022-01-05 20:25:47 +0600

    [ADD] Added make admin API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createUser, login } = require("../controller/usersController");
+const {
+	createUser,
+	login,
+	makeAdmin,
+} = require("../controller/usersController");
 
 // @route POST /api/users/login
 // @access Public
@@ -10,5 +14,9 @@ router.post("/login", login);
 // @route POST /api/users/register
 // @access Public
 router.post("/register", createUser);
+
+// @route POST /api/users/make-admin
+// @access Private
+router.post("/make-admin", makeAdmin);
 
 module.exports = router;
