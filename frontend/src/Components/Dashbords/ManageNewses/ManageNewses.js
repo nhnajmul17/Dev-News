c88988663013a9e4af695ddbd357e45bf9cc41f4
nhnajmul17 <nhnajmul17@gmail.com>
2022-01-05 10:12:27 +0600

    manage news

--- frontend/src/Components/Dashbords/ManageNewses/ManageNewses.js
+++ frontend/src/Components/Dashbords/ManageNewses/ManageNewses.js
@@ -1,3 +1,4 @@
+import { Box, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
 // import { useSelector } from "react-redux";
@@ -52,7 +53,7 @@ const ManageNewses = () => {
 		<div>
 			<h1>All News {news?.length}</h1>
 
-			<Table striped bordered hover>
+			{/* <Table striped bordered hover>
 				<thead>
 					<tr>
 						<th>#</th>
@@ -81,7 +82,37 @@ const ManageNewses = () => {
 						</tr>
 					</tbody>
 				))}
-			</Table>
+			</Table> */}
+
+			<Box sx={{ m: 3 }}>
+				<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+					{
+						news.map(pd => <Grid key={pd._id} item xs={4} sm={4} md={4}> <Card sx={{ maxWidth: 345 }}>
+							<CardMedia
+								component="img"
+								height="140"
+								image={pd.photoUrl}
+								alt="green iguana"
+							/>
+							<CardContent>
+								<Typography gutterBottom variant="h5" component="div">
+									{pd?.headLine}
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
+									Author: {pd?.Author}
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
+									Published: {pd?.PublishedDate}sq ft
+								</Typography>
+								<button onClick={() => handleDelete(pd._id)} className="btn bg-warning m-2 ">  Delete Apartment</button>
+							</CardContent>
+
+						</Card>  </Grid>)
+					}
+				</Grid>
+			</Box>
+
+
 		</div>
 
 	);
